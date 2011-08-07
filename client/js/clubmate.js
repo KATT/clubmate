7bc88dc492af6b1a95fe31edfdf50cf95e939f17
Robert Edström <robert@studsmedia.se>
2011-08-07 14:05:21 +0200

    Added libraries and skeleton

---
+++ client/js/clubmate.js
@@ -0,0 +1,64 @@
+ClubMate = new Class.Singleton({
+	
+	Postman: {},
+	UIManager: {},
+    Settings: {},
+    Strings: {},
+    DebugA: null,
+	DebugB: null,
+
+	initialize: function() {
+		ClubMate.UIManager.InitUI();
+	}
+});   
+
+
+
+/*GetHome.Postman = function() {
+	var ns = GetHome;
+	
+	var Actions = {
+		GetProviders: 10
+	};
+	
+	function ResponseHandler(response) {
+        if(response.success) {
+            try {
+                switch(response.action) {
+					case Actions.GetProviders:
+						ns.ProviderManager.HandleLoadProvidersResult(response.data);
+                    default:
+                        break;
+                }
+            } catch(e) {
+				if(console.info) {
+                	console.info(e);
+				}
+            }
+        }
+    };
+    
+    function DoRequest(page, action, params) {
+        dojo.xhrPost({
+            type: 'POST',
+            url: ns.Settings.SiteRoot + '/' + page + '/' + action + '/',
+            handleAs: 'json',
+            content: params,
+            load: function(response, ioArgs) {
+                ResponseHandler(response);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                ns.DebugA = textStatus;
+                ns.DebugB = errorThrown;
+				if(console.info) {
+					console.info(ns.Strings.GeneralError + errorThrown );
+				}
+            }
+        })
+    };
+
+	return {
+		ResponseHandler: ResponseHandler,
+        DoRequest: DoRequest
+    };
+} (); */
